21d1fe4f1400b05c8a8b5cda2bb7f31fdfc9aae1
Matt Huisman <me@matthuisman.nz>
2019-02-11 15:06:14 +1300

    update

--- resources/lib/constants.py
+++ resources/lib/constants.py
@@ -2,4 +2,4 @@ FORCE_RUN_FLAG = '_force_run'
 PLAYLIST_FILE_NAME = 'playlist.m3u8'
 EPG_FILE_NAME = 'epg.xml'
 
-PVR_ADDON_IDS = ['pvr.teleboy', 'pvr.sledovanitv.cz', 'pvr.vbox', 'pvr.zattoo']
+PVR_ADDON_IDS = ['pvr.teleboy', 'pvr.sledovanitv.cz', 'pvr.vbox', 'pvr.zattoo', 'pvr.demo']
